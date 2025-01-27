cf7d90647df9e2ac954702fb3d3cbb7d36d467c8
Akilhan <oqiljondadaxanov@gmail.com>
2025-01-27 19:25:06 +0500

    removed some pictures from gallery

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -22,24 +22,6 @@ const photos2 = [
   { src: "/gallery/15.JPG", width: 600, height: 400 },
 ];
 
-const photos3 = [
-  { src: "/gallery/11.JPG", width: 800, height: 600 },
-  { src: "/gallery/22.JPG", width: 800, height: 600 },
-  { src: "/gallery/33.JPG", width: 800, height: 600 },
-  { src: "/gallery/44.JPG", width: 800, height: 600 },
-  { src: "/gallery/55.JPG", width: 800, height: 600 },
-  { src: "/gallery/111.JPG", width: 800, height: 600 },
-];
-
-const photos4 = [
-  { src: "/gallery/66.JPG", width: 800, height: 600 },
-  { src: "/gallery/77.JPG", width: 800, height: 600 },
-  { src: "/gallery/88.JPG", width: 800, height: 600 },
-  { src: "/gallery/99.JPG", width: 800, height: 600 },
-  { src: "/gallery/222.JPG", width: 800, height: 600 },
-  { src: "/gallery/11.JPG", width: 800, height: 600 },
-];
-
 export default function Gallery() {
   useEffect(() => {
     AOS.init();
@@ -96,62 +78,6 @@ export default function Gallery() {
               />
               <div className="absolute inset-0 bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 transition duration-300 flex items-center justify-center">
                 <p className="text-white font-medium">Al-Khwarizmi 2024</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <div className="mx-auto mt-8 w-[80%] space-y-12">
-        <h1 className="text-center font-semibold text-4xl md:text-5xl lg:text-4xl text-gray-800 mt-8 mb-4 leading-snug mt-5">
-          Scenes from Muhammad al-Bukhari Olympiad 2024
-        </h1>
-
-        <div
-          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"
-          data-aos="fade-up-right"
-        >
-          {photos4.map((photo, index) => (
-            <div
-              key={index}
-              className="group relative overflow-hidden rounded-lg border border-gray-300 shadow-lg"
-            >
-              <Image
-                src={photo.src}
-                alt={`Gallery Image ${index + 1}`}
-                width={photo.width}
-                height={photo.height}
-                className="w-full h-auto transform transition duration-300 group-hover:scale-105"
-              />
-              <div className="absolute inset-0 bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 transition duration-300 flex items-center justify-center">
-                <p className="text-white font-medium">
-                  Muhammad al-Bukhari 2024
-                </p>
-              </div>
-            </div>
-          ))}
-        </div>
-
-        <div
-          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-8"
-          data-aos="fade-up-left"
-        >
-          {photos3.map((photo, index) => (
-            <div
-              key={index}
-              className="group relative overflow-hidden rounded-lg border border-gray-300 shadow-lg"
-            >
-              <Image
-                src={photo.src}
-                alt={`Gallery Image ${index + 1}`}
-                width={photo.width}
-                height={photo.height}
-                className="w-full h-auto transform transition duration-300 group-hover:scale-105"
-              />
-              <div className="absolute inset-0 bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 transition duration-300 flex items-center justify-center">
-                <p className="text-white font-medium">
-                  Muhammad al-Bukhari 2024
-                </p>
               </div>
             </div>
           ))}
