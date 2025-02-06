271bb2c72ebddb5efaaabf07654c4fd0e11961aa
Akilhan <oqiljondadaxanov@gmail.com>
2025-02-06 18:25:40 +0500

    eslint fixed

---
+++ app/uzbekistan/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Uzbekistan() {
+  return (
+    <section>
+      <div className="bg-[#0a192f] text-white p-6 sm:p-9 mb-6 sm:mb-8">
+        <h1 className="text-center text-3xl sm:text-4xl md:text-5xl font-semibold">
+          Uzbekistan
+        </h1>
+      </div>
+
+      
+    </section>
+  );
+}
