afd1f695656f81929e84fba3c7b3f9aa0df75a98
Akilhan <oqiljondadaxanov@gmail.com>
2025-02-11 16:20:24 +0500

    problems of countries removed

--- app/problems/page.tsx
+++ app/problems/page.tsx
@@ -73,24 +73,6 @@ export default function Problems() {
             </Button>
           </div>
         </div>
-
-        {/* Country Rows */}
-        <div className="mx-auto mt-6 sm:mt-8 p-4 bg-gray-100 rounded-lg max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg mb-6">
-          {["Ukraine", "Uzbekistan", "Russia", "Germany"].map((country) => (
-            <div
-              key={country}
-              className="grid grid-cols-[1fr_auto] items-center gap-4 p-4 mt-4 first:mt-0"
-            >
-              <h1 className="text-lg sm:text-xl">{country}</h1>
-              <Button
-                variant="secondary"
-                className="flex items-center bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-700 hover:scale-105 transition-all duration-300"
-              >
-                Download <FaDownload className="ml-2" />
-              </Button>
-            </div>
-          ))}
-        </div>
       </div>
     </section>
   );
