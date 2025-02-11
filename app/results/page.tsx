4a2aadcbafa4f4e0b1f96c2d100f57ec80e999ad
Akilhan <oqiljondadaxanov@gmail.com>
2025-02-12 00:48:24 +0500

    gold medalists

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -53,6 +53,30 @@ const results: ResultsData = {
       name: "Yerkebulanuly Ali",
       score: 65.7,
     },
+    {
+      rank: 4,
+      name: "Zohidjonov Elbek Elmurod o'g'li",
+      participant: "Uzbekistan Tashkent city",
+      score: 65.5,
+    },
+    {
+      rank: 5,
+      name: "Alinur Nessipbek",
+      participant: "Kazakhstan",
+      score: 64.9,
+    },
+    {
+      rank: 6,
+      name: "Jaylovov To'lqin Turdimurod o'g'li",
+      participant: "Uzbekistan Kashkadarya region",
+      score: 60.4,
+    },
+    {
+      rank: 7,
+      name: "Sailaubay Bexultan",
+      participant: "Kazakhstan",
+      score: 59.7,
+    },
   ],
   [YEARS.CURRENT]: [
     {
@@ -72,6 +96,48 @@ const results: ResultsData = {
       participant: "Turkmenistan 2nd team",
       name: "Gummanov Begench",
       score: 36,
+    },
+    {
+      rank: 4,
+      participant: "Tajikistan 1st team",
+      name: "Yusupov Mehrubon",
+      score: 36,
+    },
+    {
+      rank: 5,
+      participant: "Surkhandarya region",
+      name: "Anvarov Javoxir Chorshanbiyevich",
+      score: 33,
+    },
+    {
+      rank: 6,
+      participant: "Turkmenistan 2nd team",
+      name: "Ruslanov Murat",
+      score: 33,
+    },
+    {
+      rank: 7,
+      participant: "Kazakhstan 1st team",
+      name: "Orynbassar Berdibek",
+      score: 33,
+    },
+    {
+      rank: 8,
+      participant: "Kazakhstan 2nd team",
+      name: "Arnur Ansatov",
+      score: 31,
+    },
+    {
+      rank: 9,
+      participant: "Kyrgyzstan 1st team",
+      name: "Musakhodzhaev Sultan",
+      score: 31,
+    },
+    {
+      rank: 10,
+      participant: "Syrdarya region",
+      name: "Mamajonov Azizbek Raxmonali o'g'li",
+      score: 31,
     },
   ],
 };
