2e17edbd4bdab8ac93c2406667d1613c5a32a3cd
Okiljon <okiljondadakhonov@gmail.com>
2026-06-07 16:15:37 +0500

    DSadsA

--- data/results-2026.ts
+++ data/results-2026.ts
@@ -2,8 +2,8 @@ import { Result } from "@/types/results";
 
 export const results2026Files = {
   mathematics: {
-    href: "/results/matematics_Khimio 2026 result_final.xlsx",
-    filename: "matematics_Khimio 2026 result_final.xlsx",
+    href: "/results/matematics_Khimio 2026 result_final+.xlsx",
+    filename: "matematics_Khimio 2026 result_final+.xlsx",
     label: "Mathematics Final Results",
   },
   informatics: {
